4f04e16d6724d2867f131805629f91fb62517cf8
Caglar Turali <caglarturali@gmail.com>
2020-03-12 20:17:54 +0300

    Restructure the object

--- src/apiconfig.ts
+++ src/apiconfig.ts
@@ -1,13 +1,12 @@
+import { SortOrder } from './models/SortOrder';
+
 /**
  * Base configuration for endpoints.
  */
-import { SortOrder } from './models/SortOrder';
-
 export default {
-  limits: {
-    drawdates: 104,
-  },
-  order: {
-    drawdates: SortOrder.DESC,
+  drawdates: {
+    limit: 104,
+    skip: 0,
+    order: SortOrder.DESC,
   },
 };
