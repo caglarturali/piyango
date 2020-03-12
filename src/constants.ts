e646c95fa1369bec6c5a22c196116e8fb7e9a752
Caglar Turali <caglarturali@gmail.com>
2020-03-12 15:04:14 +0300

    Define constants

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+/**
+ * Base url for MPI web service.
+ */
+export const MPI_BASE = 'http://www.millipiyango.gov.tr/sonuclar';
+
+/**
+ * Games constants.
+ */
+export const GAMES = [
+  'sayisal',
+  'sanstopu',
+  'onnumara',
+  'superloto',
+  'piyango',
+];
+
+/**
+ * API date format.
+ */
+export const API_DATE_FORMAT = 'YYYYMMDD';
