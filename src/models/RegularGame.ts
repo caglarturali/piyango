2798c09eaa96ce6b1da324a768a4cc6cf8cde618
Caglar Turali <caglarturali@gmail.com>
2020-03-13 15:52:01 +0300

    Implement models for regular games

---
+++ src/models/RegularGame.ts
@@ -0,0 +1,65 @@
+export enum DrawType {
+  SAYISAL_LOTO = 'SAYISAL_LOTO',
+  SANS_TOPU = 'SANS_TOPU',
+  ON_NUMARA = 'ON_NUMARA',
+  SUPER_LOTO = 'SUPER_LOTO',
+}
+
+export enum MatchType {
+  $HIC = '$HIC',
+  $1_1_BILEN = '$1_1_BILEN',
+  $2_1_BILEN = '$2_1_BILEN',
+  $3_1_BILEN = '$3_1_BILEN',
+  $4_1_BILEN = '$4_1_BILEN',
+  $5_1_BILEN = '$5_1_BILEN',
+  $3_BILEN = '$3_BILEN',
+  $4_BILEN = '$4_BILEN',
+  $5_BILEN = '$5_BILEN',
+  $6_BILEN = '$6_BILEN',
+  $7_BILEN = '$7_BILEN',
+  $8_BILEN = '$8_BILEN',
+  $9_BILEN = '$9_BILEN',
+  $10_BILEN = '$10_BILEN',
+}
+
+export interface LuckyPerson {
+  oid: string;
+  kisiBasinaDusenIkramiye: number;
+  kisiSayisi: number;
+  tur: MatchType;
+}
+
+export interface LuckyProvince {
+  il: string;
+  ilView: string;
+  ilce: string;
+  ilceView: string;
+}
+
+export default interface RegularGame {
+  success: boolean;
+  data: {
+    archived: boolean | null;
+    oid: string;
+    hafta: number;
+    buyukIkramiyeKazananIl: string | null;
+    cekilisTarihi: string;
+    cekilisTuru: DrawType;
+    rakamlar: string;
+    rakamlarNumaraSirasi: string;
+    devretti: boolean;
+    devirSayisi: number | null;
+    bilenKisiler: LuckyPerson[];
+    buyukIkrKazananIlIlceler: LuckyProvince[] | null;
+    kibrisHasilati: number | null;
+    devirTutari: number;
+    kolonSayisi: number | null;
+    kdv: number | null;
+    toplamHasilat: number | null;
+    hasilat: number | null;
+    sov: number | null;
+    ikramiyeEH: number | null;
+    buyukIkramiye: number;
+    haftayaDevredenTutar: number | null;
+  };
+}
