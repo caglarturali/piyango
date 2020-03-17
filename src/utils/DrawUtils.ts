3b4e793591af5498084591020ef5590bfd5cc376
Caglar Turali <caglarturali@gmail.com>
2020-03-17 14:28:45 +0300

    Implement convertNumbersToColumn

--- src/utils/DrawUtils.ts
+++ src/utils/DrawUtils.ts
@@ -4,6 +4,28 @@ import { GameColumn } from '../models/GameColumn';
 
 export default class DrawUtils {
   /**
+   * Converts number string into a GameColumn object.
+   * @param gameId Game Id
+   * @param numbers Numbers string
+   */
+  static convertNumbersToColumn(gameId: GameID, numbers: string): GameColumn {
+    const column: GameColumn = { main: [] };
+
+    // Convert numbers string into array.
+    const numsArray = numbers.split('#').map((n) => parseInt(n, 10));
+
+    if (gameId === GameID.sanstopu) {
+      const plus = numsArray.pop();
+      column.plus = plus ? [plus] : undefined;
+    }
+
+    // Sort the rest of the numbers.
+    column.main = numsArray.sort((a, b) => a - b);
+
+    return column;
+  }
+
+  /**
    * Returns winning numbers as GameColumn object
    * @param gameId Game Id
    * @param drawDetails Draw details
@@ -12,22 +34,8 @@ export default class DrawUtils {
     gameId: GameID,
     drawDetails: RegularDraw,
   ): GameColumn {
-    const winningNumbers: GameColumn = { main: [] };
-    if (gameId === GameID.piyango) return winningNumbers;
-
+    if (gameId === GameID.piyango) return { main: [] };
     const { rakamlar } = drawDetails;
-
-    // Convert rakamlar into array.
-    const numsArray = rakamlar.split('#').map((n) => parseInt(n, 10));
-
-    if (gameId === GameID.sanstopu) {
-      const plus = numsArray.pop();
-      winningNumbers.plus = plus ? [plus] : undefined;
-    }
-
-    // Sort the rest of the numbers.
-    winningNumbers.main = numsArray.sort((a, b) => a - b);
-
-    return winningNumbers;
+    return this.convertNumbersToColumn(gameId, rakamlar);
   }
 }
