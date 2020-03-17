be38aa2fa4899ab3746b421c5b55e68c6a872fc2
Caglar Turali <caglarturali@gmail.com>
2020-03-17 14:44:01 +0300

    Add delimiter

--- src/utils/DrawUtils.ts
+++ src/utils/DrawUtils.ts
@@ -12,7 +12,7 @@ export default class DrawUtils {
     const column: GameColumn = { main: [] };
 
     // Convert numbers string into array.
-    const numsArray = numsStr.split(/#|-|\+|_/).map((n) => parseInt(n, 10));
+    const numsArray = numsStr.split(/#|-|\+|_|,/).map((n) => parseInt(n, 10));
 
     if (gameId === GameID.sanstopu) {
       const plus = numsArray.pop();
