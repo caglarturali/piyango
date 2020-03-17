b08d9dbbcc867adf71cf7f50c3b443080ec47453
Caglar Turali <caglarturali@gmail.com>
2020-03-17 14:42:13 +0300

    Rename param to be more specific

--- src/utils/DrawUtils.ts
+++ src/utils/DrawUtils.ts
@@ -6,13 +6,13 @@ export default class DrawUtils {
   /**
    * Converts number string into a GameColumn object.
    * @param gameId Game Id
-   * @param numbers Numbers string
+   * @param numsStr Numbers string
    */
-  static convertNumbersToColumn(gameId: GameID, numbers: string): GameColumn {
+  static convertNumbersToColumn(gameId: GameID, numsStr: string): GameColumn {
     const column: GameColumn = { main: [] };
 
     // Convert numbers string into array.
-    const numsArray = numbers.split(/#|-|\+|_/).map((n) => parseInt(n, 10));
+    const numsArray = numsStr.split(/#|-|\+|_/).map((n) => parseInt(n, 10));
 
     if (gameId === GameID.sanstopu) {
       const plus = numsArray.pop();
