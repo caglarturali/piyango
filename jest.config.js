0a77d440cff7ed7722e90c80a56fd8b732ecbb9b
Caglar Turali <caglarturali@gmail.com>
2020-03-18 20:41:47 +0300

    Tweak jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testTimeout: 7000,
+  bail: 1,
+  globals: {
+    'ts-jest': {
+      packageJson: 'package.json',
+    },
+  },
 };
