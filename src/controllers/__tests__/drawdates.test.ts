a178eef22fa1d016f81369a457c18b8b9d894257
Caglar Turali <caglarturali@gmail.com>
2020-03-18 20:55:25 +0300

    Write basic tests for drawdates

---
+++ src/controllers/__tests__/drawdates.test.ts
@@ -0,0 +1,41 @@
+import moment from 'moment';
+import { getDrawDates } from '../drawdates';
+import { GameID } from '../../models/Game';
+import { SortOrder } from '../../models/SortOrder';
+import { DATE_FORMAT } from '../../constants';
+
+test('should successfully get draw dates for given game', async () => {
+  const limit = 10;
+  const { success, statusCode, data, error } = await getDrawDates(
+    GameID.sayisal,
+    limit,
+    0,
+    SortOrder.DESC,
+  );
+  expect(statusCode).toBe(200);
+  expect(success).toBeTruthy();
+  expect(data).toBeDefined();
+  expect(data).toHaveLength(limit);
+  expect(error).toBeNull();
+});
+
+test('should respond with error when invalid gameId is given', async () => {
+  const fakeGameId = 'sayisalx' as GameID;
+  const { error, statusCode, success } = await getDrawDates(
+    fakeGameId,
+    10,
+    0,
+    SortOrder.DESC,
+  );
+  expect(success).toBeFalsy();
+  expect(statusCode).not.toBe(200);
+  expect(error).toBeDefined();
+});
+
+test('should sort results in specified order', async () => {
+  const { data } = await getDrawDates(GameID.onnumara, 2, 0, SortOrder.ASC);
+  const [first, second] = data;
+  const firstMoment = moment(first.tarih, DATE_FORMAT).unix();
+  const secondMoment = moment(second.tarih, DATE_FORMAT).unix();
+  expect(firstMoment).toBeLessThan(secondMoment);
+});
