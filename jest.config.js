fcfef420953cbdeacd5ab34fb3e4b2b8babc4e95
Caglar Turali <caglarturali@gmail.com>
2020-03-18 21:38:52 +0300

    Move coverageDirectory pref to jest.config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   testTimeout: 7000,
   bail: 1,
+  coverageDirectory: 'output/coverage/jest',
   globals: {
     'ts-jest': {
       packageJson: 'package.json',
