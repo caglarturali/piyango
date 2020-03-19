de7ddbbc1f33d3fffbda63818e7c381231892e8b
Caglar Turali <caglarturali@gmail.com>
2020-03-19 13:08:09 +0300

    Wite tests for embed

---
+++ src/controllers/__tests__/embed.test.ts
@@ -0,0 +1,25 @@
+import { getEmbeddableStream } from '../embed';
+import { GameID } from '../../models/Game';
+import { STREAM_URL } from '../../constants';
+
+test('should successfully return embeddable stream for valid draw', async () => {
+  const { status, contents, error } = await getEmbeddableStream(
+    GameID.sanstopu,
+    '20200311',
+  );
+  expect(status).toBe(200);
+  expect(contents).toBeDefined();
+  expect(contents).toContain(STREAM_URL.base.url);
+  expect(error).toBeUndefined();
+});
+
+test('should successfully return dummy stream for invalid draw', async () => {
+  const { status, contents, error } = await getEmbeddableStream(
+    GameID.sanstopu,
+    '20200312',
+  );
+  expect(status).toBe(200);
+  expect(contents).toBeDefined();
+  expect(contents).toContain(STREAM_URL.dummy.url);
+  expect(error).toBeUndefined();
+});
