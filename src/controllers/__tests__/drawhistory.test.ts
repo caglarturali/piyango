1ece0b4f793df604a77c7f8e971611d2b9df0f8f
Caglar Turali <caglarturali@gmail.com>
2020-03-19 12:52:30 +0300

    Write tests for drawhistory

---
+++ src/controllers/__tests__/drawhistory.test.ts
@@ -0,0 +1,28 @@
+import { getDrawHistory, getDrawHistoryForGame } from '../drawhistory';
+import { GAMES } from '../../constants';
+import { GameID } from '../../models/Game';
+import { isDateValid } from '../../utils';
+
+test('should get draw history for all games for given date', async () => {
+  const dateStr = '20200101';
+  const { data } = await getDrawHistory(dateStr);
+
+  expect(data).toHaveLength(GAMES.length);
+  data.forEach(({ gameId, draws }) => {
+    expect(gameId).toBeDefined();
+    expect(draws).toBeDefined();
+    draws.forEach((date) => {
+      expect(isDateValid(date)).toBeTruthy();
+    });
+  });
+});
+
+test('should get draw history for given game and date', async () => {
+  const dateStr = '20191231';
+  const { data } = await getDrawHistoryForGame(dateStr, GameID.piyango);
+
+  expect(data.length).toBeGreaterThan(0);
+  data.forEach((date) => {
+    expect(isDateValid(date)).toBeTruthy();
+  });
+});
