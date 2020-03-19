ff421f0d2b110849cfa92e8757cf38b8564d2b44
Caglar Turali <caglarturali@gmail.com>
2020-03-19 14:22:08 +0300

    Write tests for check

---
+++ src/controllers/__tests__/check.test.ts
@@ -0,0 +1,61 @@
+import { checkNumbers } from '../check';
+import { GameID } from '../../models/Game';
+import CheckBody from '../../models/CheckBody';
+import { RegularCheckResult } from '../../models/RegularDraw';
+import { LotteryCheckResult } from '../../models/LotteryDraw';
+
+test('should check numbers successfully for a regular draw', async () => {
+  const body: CheckBody = {
+    numbers: ['05#20#24#25#32#34', '07#24#25#08#10#51'],
+  };
+
+  const { statusCode, success, data } = await checkNumbers(
+    GameID.superloto,
+    '20200312',
+    body,
+  );
+
+  expect(statusCode).toBe(200);
+  expect(success).toBeTruthy();
+  expect(data).toHaveLength(body.numbers.length);
+  data.forEach((res) => {
+    const result = res as RegularCheckResult;
+    expect(typeof result.prize).toBe('number');
+  });
+});
+
+test('should check numbers successfully for a lottery draw', async () => {
+  const body: CheckBody = {
+    numbers: ['501222', '511222', '123180', '221384', '123450'],
+  };
+
+  const { statusCode, success, data } = await checkNumbers(
+    GameID.piyango,
+    '20200309',
+    body,
+  );
+
+  expect(statusCode).toBe(200);
+  expect(success).toBeTruthy();
+  expect(data).toHaveLength(body.numbers.length);
+  data.forEach((res) => {
+    const result = res as LotteryCheckResult;
+    expect(typeof result.prize).toBe('number');
+  });
+});
+
+test('should respond with an error if length of the numbers are not correct', async () => {
+  const body: CheckBody = {
+    numbers: ['05#20#24#25#32#34#12#23'],
+  };
+
+  const { statusCode, success, error } = await checkNumbers(
+    GameID.superloto,
+    '20200312',
+    body,
+  );
+
+  expect(statusCode).not.toBe(200);
+  expect(success).toBeFalsy();
+  expect(error).toBeDefined();
+});
