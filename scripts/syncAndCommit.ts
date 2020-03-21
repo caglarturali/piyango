69a27c9e96490831f6352e77eeec709704c565a6
Caglar Turali <caglarturali@gmail.com>
2020-03-21 18:38:06 +0300

    Tweak scripts

--- scripts/syncAndCommit.ts
+++ scripts/syncAndCommit.ts
@@ -4,7 +4,7 @@
 import { execSync } from 'child_process';
 const msg = 'SYNC: ' + (process.argv[2] || new Date().toLocaleString());
 execSync(
-  `npm run sync:draws && git add static/draws && git commit -m "${msg}"`,
+  `npm run sync:draws && npm run sync:stats && git add static/ && git commit -m "${msg}"`,
   {
     stdio: [0, 1, 2],
   },
