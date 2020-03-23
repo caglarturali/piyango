9185a81eb5c94df640eadfa39fd54240bf087d34
Caglar Turali <caglarturali@gmail.com>
2020-03-23 15:06:20 +0300

    Write tests for generate

---
+++ src/controllers/__tests__/generate.test.ts
@@ -0,0 +1,23 @@
+import { generateGuesses } from '../generate';
+import { GameID, NumbersPool, RegularGame } from '../../models/Game';
+import { GAMES } from '../../constants';
+
+test('should successfully generate random numbers for given game', async () => {
+  const gameId = GameID.sanstopu;
+  const colCount = 10;
+  const game = GAMES.find((g) => g.id === gameId) as RegularGame;
+
+  const { select: mainCount } = game.pool.main;
+  const { select: plusCount } = game.pool.plus as NumbersPool;
+
+  const { statusCode, data } = await generateGuesses(gameId, colCount);
+
+  expect(statusCode).toBe(200);
+  expect(data).toHaveLength(colCount);
+  data.forEach((col) => {
+    expect(col.main).toBeDefined();
+    expect(col.plus).toBeDefined();
+    expect(col.main).toHaveLength(mainCount);
+    expect(col.plus).toHaveLength(plusCount);
+  });
+});
