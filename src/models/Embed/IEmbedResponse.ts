879e2a86064ae4512220820faf1357980f028ef3
Caglar Turali <caglarturali@gmail.com>
2020-03-25 15:59:23 +0300

    Define type Header

--- src/models/Embed/IEmbedResponse.ts
+++ src/models/Embed/IEmbedResponse.ts
@@ -1,7 +1,8 @@
 import IResponse from '../IResponse';
+import { Header } from '../Header';
 
 export interface IEmbedResponse extends IResponse {
   status: number;
   contents?: string;
-  header?: [string, string];
+  header?: Header;
 }
