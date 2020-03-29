9337711235d97b2bcf0996208ff68a603765a062
Caglar Turali <caglarturali@gmail.com>
2020-03-29 17:59:15 +0300

    Implement SectionHeader

---
+++ src/SectionHeader/index.tsx
@@ -0,0 +1,33 @@
+/**
+ * SectionHeader component.
+ */
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import Grid from '@material-ui/core/Grid';
+import styles from './styles';
+
+const useStyles = makeStyles(styles);
+
+export interface SectionHeaderProps {
+  title: string;
+  lg?: any;
+}
+
+export const SectionHeader: React.FunctionComponent<SectionHeaderProps> = ({
+  title,
+  lg,
+  children,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Grid container>
+      <Grid item xs={12} lg={lg}>
+        <div className={classes.root}>
+          <span className={classes.title}>{title}</span>
+          {children && <section>{children}</section>}
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
