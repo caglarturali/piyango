db1e6734fe18859042e2eb875ec4287d46e31621
Caglar Turali <caglarturali@gmail.com>
2020-04-03 20:05:29 +0300

    Move DateUtils into common package

--- packages/server/src/utils/index.ts
+++ packages/server/src/utils/index.ts
@@ -1,5 +1,4 @@
-import DateUtils from './DateUtils';
 import NumUtils from './NumUtils';
 import PathUtils from './PathUtils';
 
-export { DateUtils, NumUtils, PathUtils };
+export { NumUtils, PathUtils };
