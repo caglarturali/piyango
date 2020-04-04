64fbd043fada3da2af5f2ede9b8fc6c7ff1f549e
Caglar Turali <caglarturali@gmail.com>
2020-04-04 16:04:15 +0300

    Use hooks instead of HOCs

--- packages/app/src/components/Container/index.tsx
+++ packages/app/src/components/Container/index.tsx
@@ -2,21 +2,18 @@
  * Container component.
  */
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
-import withMobileDialog, {
-  InjectedProps,
-} from '@material-ui/core/withMobileDialog';
+import { makeStyles, useMediaQuery } from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import styles from './styles';
 
 const useStyles = makeStyles(styles);
 
-const Container: React.FunctionComponent<InjectedProps> = ({
-  children,
-  fullScreen,
-}) => {
+const Container: React.FunctionComponent = ({ children }) => {
+  const theme = useTheme();
   const classes = useStyles();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('xs'));
 
   // Decide spacing based on screen size.
   const spacing = fullScreen ? 0 : 2;
@@ -30,4 +27,4 @@ const Container: React.FunctionComponent<InjectedProps> = ({
   );
 };
 
-export default withMobileDialog({ breakpoint: 'xs' })(Container);
+export default Container;
