ca5726ae4beacfdb5b660704123eea239ed5967d
Caglar Turali <caglarturali@gmail.com>
2020-04-05 15:15:14 +0300

    Move Check models to package common

--- packages/server/src/models/Check/index.ts
+++
@@ -1,6 +0,0 @@
-import LotteryCheck from './LotteryCheck';
-import RegularCheck from './RegularCheck';
-
-export { LotteryCheck, RegularCheck };
-export * from './ICheckNumbers';
-export * from './CheckResult';
