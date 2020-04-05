8378376656c58ef11ab405e24d0db0a9379f9664
Caglar Turali <caglarturali@gmail.com>
2020-04-05 17:31:19 +0300

    Fetch draw data when draw date is changed

--- packages/app/src/components/CheckCoupon/index.tsx
+++ packages/app/src/components/CheckCoupon/index.tsx
@@ -1,16 +1,23 @@
 /**
  * CheckCoupon component.
  */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core';
 import {
+  DrawDataType,
   DrawDate,
   Game,
   GameColumn,
   RegularGame,
 } from '@caglarturali/piyango-common';
 import CheckCouponLayout from './comps/Layout';
-import { DrawDatePanel, ReportPanel, UserNumbersPanel } from './comps/Panels';
+import {
+  DrawDatePanel,
+  PanelID,
+  ReportPanel,
+  UserNumbersPanel,
+} from './comps/Panels';
+import API from '../../services/API';
 import styles from './styles';
 
 const useStyles = makeStyles(styles);
@@ -27,9 +34,18 @@ const CheckCoupon: React.FunctionComponent<CheckCouponProps> = ({
   const classes = useStyles();
 
   const [open, setOpen] = useState(true);
-  const [panelExpanded, setPanelExpanded] = useState('user-numbers');
+  const [panelExpanded, setPanelExpanded] = useState(PanelID.UserNumbers);
   const [drawDate, setDrawDate] = useState<DrawDate>(drawDateProp);
+  const [drawData, setDrawData] = useState<DrawDataType>();
   const [userNumbers, setUserNumbers] = useState<GameColumn[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await API.getDrawDetails(game.id, drawDate);
+      setDrawData(data);
+    };
+    getData();
+  }, [drawDate]);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -51,30 +67,34 @@ const CheckCoupon: React.FunctionComponent<CheckCouponProps> = ({
       handleReset={() => {}}
     >
       <DrawDatePanel
-        id="draw-date"
+        id={PanelID.DrawDate}
         heading="Çekiliş Tarihi"
         onPanelChange={setPanelExpanded}
         expandedPanel={panelExpanded}
         game={game}
         selectedDate={drawDate}
         onDateChange={setDrawDate}
+        drawData={drawData}
       />
       <UserNumbersPanel
-        id="user-numbers"
+        id={PanelID.UserNumbers}
         heading="Tahminleriniz"
         onPanelChange={setPanelExpanded}
         expandedPanel={panelExpanded}
         game={game as RegularGame}
         onAddUserNumbers={handleAddUserNumbers}
         isReportAvailable={userNumbers.length > 0}
-        onReportClick={() => setPanelExpanded('report-panel')}
+        onReportClick={() => setPanelExpanded(PanelID.Report)}
       />
       <ReportPanel
-        id="report-panel"
+        id={PanelID.Report}
         heading="Rapor"
         disabled={userNumbers.length === 0}
         onPanelChange={setPanelExpanded}
         expandedPanel={panelExpanded}
+        game={game}
+        drawData={drawData}
+        userNumbers={userNumbers}
       />
     </CheckCouponLayout>
   );
