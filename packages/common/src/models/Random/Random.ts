6799ffe15a86aea384aef2d6171e71719c6098e0
Caglar Turali <caglarturali@gmail.com>
2020-04-06 15:59:34 +0300

    Fix: lint?

--- packages/common/src/models/Random/Random.ts
+++ packages/common/src/models/Random/Random.ts
@@ -26,10 +26,9 @@ export class Random {
       const { main, plus } = this.game.pool;
 
       // Generate numbers.
-      const col =
-        {
-          main: this.generateNumbers(main),
-        } as GameColumn;
+      const col = {
+        main: this.generateNumbers(main),
+      } as GameColumn;
 
       if (plus) {
         col.plus = this.generateNumbers(plus);
