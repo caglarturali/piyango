eb4cd95225fd6b7949a05fe9dc6897cedaa7487c
Caglar Turali <caglarturali@gmail.com>
2020-04-06 16:13:58 +0300

    Export match dir

--- packages/common/src/models/index.ts
+++ packages/common/src/models/index.ts
@@ -2,6 +2,7 @@ export * from './Check';
 export * from './Draw';
 export * from './Game';
 export * from './Lottery';
+export * from './Match';
 export * from './ProcessDraw';
 export * from './Random';
 export * from './Regular';
