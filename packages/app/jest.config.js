4a116695d7d82f0e62acaa29474bf03da9a3a46f
Caglar Turali <caglarturali@gmail.com>
2020-04-09 22:48:06 +0300

    Improve testing setup :blush:

--- packages/app/jest.config.js
+++ packages/app/jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   ...require('../../jest.config.js'),
   testEnvironment: 'jsdom',
-  setupFiles: ['<rootDir>/src/setupTests.ts'],
-  setupFilesAfterEnv: [],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  setupFiles: [],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
 };
