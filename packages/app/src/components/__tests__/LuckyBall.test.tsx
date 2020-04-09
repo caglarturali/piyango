511522a1d3125d8d369e7d7be2a9aa003541b5db
Caglar Turali <caglarturali@gmail.com>
2020-04-09 22:25:31 +0300

    Improve tests

--- packages/app/src/components/__tests__/LuckyBall.test.tsx
+++ packages/app/src/components/__tests__/LuckyBall.test.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import LuckyBall from '../LuckyBall';
 
 describe('<LuckyBall />', () => {
   test('renders LuckyBall component with padded number', () => {
-    const { asFragment } = render(<LuckyBall num={7} />);
+    const { asFragment, getByText } = render(<LuckyBall num={7} />);
+    const str = getByText(/07/);
     expect(asFragment()).toMatchSnapshot();
+    expect(str).toBeInTheDocument();
   });
 
   test('renders LuckyBall component w/o padding on number', () => {
-    const { asFragment } = render(<LuckyBall num={7} pad={false} />);
+    const { asFragment, getByText, queryByText } = render(
+      <LuckyBall num={7} pad={false} />,
+    );
+    const str = getByText(/7/);
+    const padded = queryByText(/07/);
     expect(asFragment()).toMatchSnapshot();
+    expect(str).toBeInTheDocument();
+    expect(padded).toBeNull();
   });
 });
