6baed904574d64443ccf276aad6f30ed3bfbe88f
Caglar Turali <caglarturali@gmail.com>
2020-04-09 19:45:18 +0300

    Derive jest config from root config

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testTimeout: 15000,
-  bail: 1,
-  coverageDirectory: 'output/coverage/jest',
-  globals: {
-    'ts-jest': {
-      packageJson: 'package.json',
-    },
-  },
+  ...require('../../jest.config.js'),
 };
