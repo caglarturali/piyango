806865df0943985b907e122e5ca2a80141c5fffc
Caglar Turali <caglarturali@gmail.com>
2020-04-18 17:31:18 +0300

    Restructure file contents

--- packages/app/src/shared/urls.ts
+++ packages/app/src/shared/urls.ts
@@ -1,9 +1,9 @@
+import { app } from '../configs';
+
 /**
  * Base url of the production server.
  */
-import { app } from '../configs';
-
-const { production, development } = app.apiUrl;
-
 export const API_BASE =
-  process.env.NODE_ENV === 'production' ? production : development;
+  process.env.NODE_ENV === 'production'
+    ? app.apiUrl.production
+    : app.apiUrl.development;
