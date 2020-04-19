7e4dd7c90d5883d40d32af167225ab9fb7e53e55
Caglar Turali <caglarturali@gmail.com>
2020-04-19 20:43:42 +0300

    Use only the api to get the comment count

--- packages/app/src/containers/Disqussion/components/Count/index.tsx
+++ packages/app/src/containers/Disqussion/components/Count/index.tsx
@@ -5,9 +5,6 @@ import React, { useEffect, useState } from 'react';
 import { DrawDate, Game } from '@caglarturali/piyango-common';
 import { makeStyles } from '@material-ui/core';
 import Badge from '@material-ui/core/Badge';
-import { CommentCount } from 'disqus-react';
-import { disqus } from '../../../../configs';
-import { getDisqusConfig } from '../../../../helpers';
 import API from '../../../../services/API';
 import styles from './styles';
 
@@ -31,7 +28,7 @@ export const Count: React.FunctionComponent<CountProps> = ({
   useEffect(() => {
     const getData = async () => {
       const commentCount = await API.getCommentsCount(game.id, drawDate);
-      if (commentCount > 0) setCount(commentCount);
+      setCount(commentCount);
       setLoading(false);
     };
     getData();
@@ -39,12 +36,6 @@ export const Count: React.FunctionComponent<CountProps> = ({
 
   return (
     <>
-      <span className={classes.originalCount}>
-        <CommentCount
-          config={getDisqusConfig(game, drawDate)}
-          shortname={disqus.shortname}
-        />
-      </span>
       {loading ? (
         <>{children}</>
       ) : (
@@ -52,7 +43,6 @@ export const Count: React.FunctionComponent<CountProps> = ({
           color="secondary"
           badgeContent={count}
           max={999}
-          showZero
           classes={{ badge: classes.badge }}
         >
           {children}
